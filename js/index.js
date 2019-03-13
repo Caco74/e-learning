c16298adb0fc9f8d9811622ddf5e7890417a5c93
Franco Daniel Demetrio <francodemetrio87@gmail.com>
2019-03-12 23:20:40 -0300

    versión 1.0

---
+++ js/index.js
@@ -0,0 +1,26 @@
+//
+// function cambiarFondo(background) {
+//   document.getElementsByTagName('body')[0].style.backgroundColor= "#4d62d0"  ;
+//   var hijo = document.getElementsByTagName("body")[0].children[0];
+// }
+//
+// function cambiarFondo(background) {
+//   document.getElementsByTagName('body')[0].style.backgroundColor= "#149c5f"  ;
+//   if (body.children[0]) {
+//     document.getElementsByTagName('body.children[0]')[0].style.backgroundColor= "inherit";
+//   }
+// }
+//
+// function tamañoPanel() {
+//   document.querySelector("div[class='item']").style.width= "4%";
+//   document.querySelector("div[class='item']").style.backgroundColor= "#4d62d0";
+//
+// }
+function colorUno() {
+  document.getElementsByTagName('h1')[0].style.backgroundColor = "#4d62d0";
+}
+colorUno();
+function colorDos() {
+  document.getElementsByClassName('container-botones')[0].style.backgroundColor = "#149c5f";
+}
+colorDos();
